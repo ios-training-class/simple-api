e3bfe889cea4aca4dc6b1eca9a647968632ac0bf
tommy <nguyentien8x@gmail.com>
2017-03-02 14:44:15 +0700

    Update quotes api

--- apis/quotes.js
+++ apis/quotes.js
@@ -8,9 +8,17 @@ router.get('/hello', function(req, res) {
   res.send('hello world');
 });
 
+router.post('/hello', function(req, res) {
+  res.send('hello world from post method');
+});
+
 router.get('/hello1', function(req, res) {
   res.send(JSON.stringify({'hello': 'world'}));
 });
+
+router.get('/:code', function(req, res) {
+  res.status(req.params.code).send('Status code:' + req.params.code);
+})
 
 router.get('/', function(req, res) {
   axios.get(API_QUOTE, {
