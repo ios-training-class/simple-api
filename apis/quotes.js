844ce1686f2992387bed2160d8b9a07daf33188a
tommy <nguyentien8x@gmail.com>
2017-03-02 14:04:20 +0700

    Update quotes api

--- apis/quotes.js
+++ apis/quotes.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var axios = require('axios');
+var logger = require('../helpers/logger');
 var router = express.Router();
+var API_QUOTE = 'http://quotesondesign.com/wp-json/posts';
 
 router.get('/', function(req, res) {
-    res.send(JSON.stringify({'hello':'world'}));
+    axios.get(API_QUOTE, {
+		  params: {
+		    'filter[orderby]': 'rand',
+        'filter[posts_per_page]': 1,
+        'timestamp': new Date().getTime()
+		  }
+    })
+		.then(function (response) {
+	    return response.data;
+	  })
+	   .then(function (response) {
+	    res.send(response);
+	  })
+	  .catch(function (error) {
+	    logger.error(error);
+	  });
 });
 
 module.exports = router;
