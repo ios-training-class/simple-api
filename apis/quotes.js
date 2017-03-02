fc69b04e89c7e9a9a2feddde03fa4c55a25e27e7
tommy <nguyentien8x@gmail.com>
2017-03-02 14:33:32 +0700

    Update quotes api

--- apis/quotes.js
+++ apis/quotes.js
@@ -4,23 +4,44 @@ var logger = require('../helpers/logger');
 var router = express.Router();
 var API_QUOTE = 'http://quotesondesign.com/wp-json/posts';
 
+router.get('/hello', function(req, res) {
+  res.send('hello world');
+});
+
+router.get('/hello1', function(req, res) {
+  res.send(JSON.stringify({'hello': 'world'}));
+});
+
 router.get('/', function(req, res) {
-    axios.get(API_QUOTE, {
-		  params: {
-		    'filter[orderby]': 'rand',
-        'filter[posts_per_page]': 1,
-        'timestamp': new Date().getTime()
-		  }
-    })
-		.then(function (response) {
-	    return response.data;
-	  })
-	   .then(function (response) {
-	    res.send(response);
-	  })
-	  .catch(function (error) {
-	    logger.error(error);
-	  });
+  axios.get(API_QUOTE, {
+	  params: {
+	    'filter[orderby]': req.query.orderby || 'rand',
+      'filter[posts_per_page]': req.query.posts_per_page || 1,
+      'timestamp': new Date().getTime()
+	  }
+  })
+	.then(function (response) {
+    return response.data;
+  })
+   .then(function (response) {
+    res.send(response);
+  })
+  .catch(function (error) {
+    logger.error(error);
+  });
+});
+
+router.get('/:id(\\d+)', function(req, res) {
+  axios.get(API_QUOTE + '/' + req.params['id'])
+	.then(function (response) {
+    return response.data;
+  })
+   .then(function (response) {
+    res.send(response);
+  })
+  .catch(function (error) {
+    logger.error(error);
+  });
 });
 
 module.exports = router;
